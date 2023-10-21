4b689e02bf7568acf992cf564bb028839ee21fcb
Boopiboop <eivind-buodd@hotmail.com>
2023-10-21 06:36:58 +0200

    New way to collect names
    Before: Names were collected from the DOM, relying on webpage structure and only capturing visible usernames.
    
    Now: Using tmi.js, names are collected from chat messages, making it robust against webpage changes and capturing all participating users.

--- content.js
+++ content.js
@@ -1,15 +1,16 @@
-const CHAT_CONTAINER_SELECTOR = "#live-page-chat > div > div > div.Layout-sc-1xcs6mc-0.gTweXu.chat-shell.chat-shell__expanded > div > div > section > div > seventv-container > div > div:nth-child(2) > div.Layout-sc-1xcs6mc-0.bGbtMS.chat-input__buttons-container";
-const CHAT_ELEMENTS_SELECTOR = "#seventv-message-container > main > div > span > div > div > span > div.seventv-chat-user > span.seventv-chat-user-username > span > span";
-const NAMES_LIMIT = 20;
+const CHAT_CONTAINER_SELECTOR = ".chat-input__buttons-container";
+const CHAT_ELEMENTS_SELECTOR = ".seventv-chat-user-username";
 
 class ChatObserver {
   constructor() {
     this.observer = new MutationObserver(() => this.observeChat());
     this.observer.observe(document.body, { childList: true, subtree: true });
-    this.checkTokenAndUsername();
     this.chatContainer = null;
     this.token = null;
     this.username = null;
+    this.channel = null;
+    this.uniqueNames = [];
+    this.checkTokenAndUsername();
   }
 
   checkTokenAndUsername() {
@@ -19,24 +20,86 @@ class ChatObserver {
       if (!this.token || !this.username) {
         chrome.runtime.sendMessage({action: "openOptionsPage"});
       } else {
-        this.observeChat();
+        try {
+          this.connectToChat();
+        } catch (error) {
+          console.error("Failed to connect to chat due to:", error);
+        }
       }
     });
   }
 
-  observeChat() {
-    this.chatContainer = this.chatContainer || this.getChatContainer();
-    const existingButtonContainer1 = this.chatContainer?.querySelector("div.Layout-sc-1xcs6mc-0.jaMZlX");
-    const existingButtonContainer2 = this.chatContainer?.querySelector("div.Layout-sc-1xcs6mc-0.liBNWc");
-
-    if (this.chatContainer && existingButtonContainer1 && existingButtonContainer2) {
-      this.observer.disconnect();
-      const alertButtonContainer = this.createAlertButtonContainer();
-      const alertButton = this.createAlertButton();
-
-      alertButtonContainer.appendChild(alertButton);
-      existingButtonContainer1.insertAdjacentElement("afterend", alertButtonContainer);
+  collectNames(username) {
+    const englishLettersRegex = /^[a-zA-Z0-9\s\-_]+$/;
+    const blacklist = ["schnozebot", "fossabot", "biroman", "xqc", "thepositivebot", "darkface____"]; 
+  
+    if (username && englishLettersRegex.test(username) && !blacklist.includes(username.toLowerCase())) {
+      if (!this.uniqueNames.includes(username)) {
+        if (this.uniqueNames.length >= 25) {
+          const removedUsername = this.uniqueNames.shift();
+          console.log(`%c${removedUsername}%c replaced by %c${username}`, 'color: red', 'color: black', 'color: green');
+        } else {
+          console.log(`%c${username} was added`, 'color: green');
+        }
+        this.uniqueNames.push(username);
+      }
     }
+  }
+  
+  observeChat() {
+    try {
+      this.chatContainer = this.chatContainer || this.getChatContainer();
+      const existingButtonContainer1 = this.chatContainer?.querySelector("div.Layout-sc-1xcs6mc-0.jaMZlX");
+      const existingButtonContainer2 = this.chatContainer?.querySelector("div.Layout-sc-1xcs6mc-0.liBNWc");
+  
+      if (this.chatContainer && existingButtonContainer1 && existingButtonContainer2) {
+        this.observer.disconnect();
+        const alertButtonContainer = this.createAlertButtonContainer();
+        const alertButton = this.createAlertButton();
+  
+        alertButtonContainer.appendChild(alertButton);
+        existingButtonContainer1.insertAdjacentElement("afterend", alertButtonContainer);
+      }
+    } catch (error) {
+      console.error("Failed to create alert button due to:", error);
+    }
+  }
+
+  connectToChat() {
+    const url = window.location.href;
+    this.channel = new URL(url).pathname.slice(1);
+    try {
+      this.collectNames();
+    } catch (error) {
+      console.error("Failed to collect names due to:", error);
+    }
+
+    const client = new tmi.Client({
+      options: { debug: false },
+      identity: {
+        username: this.username,
+        password: this.token,
+      },
+      channels: [this.channel],
+    });
+
+    client.connect().catch(error => {
+      console.error("Failed to connect to the chat:", error);
+      // Try to reconnect
+      try {
+        client.connect();
+      } catch (reconnectError) {
+        console.error("Failed to reconnect to the chat:", reconnectError);
+      }
+    });
+
+    client.on('message', (channel, userstate, message, self) => {
+      try {
+        this.collectNames(userstate.username);
+      } catch (error) {
+        console.error("Failed to collect names from message due to:", error);
+      }
+    });
   }
 
   getChatContainer() {
@@ -66,9 +129,8 @@ class ChatObserver {
     bellIcon.addEventListener("mouseover", () => this.previewAlert(bellIcon));
   
     setInterval(() => {
-      const collectedNames = this.collectNames();
-      const collectedNamesCount = collectedNames.split(' ').length - 2; // Subtract 2 for the two words in "SOYSCREAM ALERT"
-      countText.textContent = `${collectedNamesCount}x`;
+      const collectedNamesCount = this.uniqueNames.size;
+      countText.textContent = `${this.uniqueNames.length}x`;
       bellIcon.textContent = "🔔";
     }, 1000);
   
@@ -76,50 +138,48 @@ class ChatObserver {
   }
 
   sendAlert() {
-    const collectedNames = this.collectNames();
-    console.log(collectedNames);
+    try {
+      const collectedNames = Array.from(this.uniqueNames).join(" ");
+      console.log(collectedNames);
   
-    const url = window.location.href;
-    const channelName = new URL(url).pathname.slice(1);
+      const url = window.location.href;
+      const channelName = new URL(url).pathname.slice(1);
   
-    const client = new tmi.Client({
-      options: { debug: false },
-      identity: {
-        username: this.username,
-        password: this.token,
-      },
-      channels: [channelName],
-    });
+      const client = new tmi.Client({
+        options: { debug: false },
+        identity: {
+          username: this.username,
+          password: this.token,
+        },
+        channels: [channelName],
+      });
   
-    client.connect();
+      client.connect().catch(error => {
+        console.error("Failed to connect to the chat:", error);
+        // Try to reconnect
+        try {
+          client.connect();
+        } catch (reconnectError) {
+          console.error("Failed to reconnect to the chat:", reconnectError);
+        }
+      });
   
-    client.on("connected", () => {
-      client.say(channelName, collectedNames);
-    });
+      client.on("connected", () => {
+        client.say(channelName, `${collectedNames} \n\nSOYSCREAM ALERT`).catch(error => {
+          console.error("Failed to send message:", error);
+        });
+      });
+    } catch (error) {
+      console.error("An error occurred in sendAlert:", error);
+    }
   }
 
   previewAlert(alertButton) {
-    const previewMessage = this.collectNames();
+    const previewMessage = Array.from(this.uniqueNames).join(" ");
     console.log(`Preview: ${previewMessage}`);
   }
 
-  collectNames(limit = NAMES_LIMIT) {
-    const chatElements = document.querySelectorAll(CHAT_ELEMENTS_SELECTOR);
-    const uniqueNames = new Set();
-    const englishLettersRegex = /^[a-zA-Z0-9\s\-_]+$/;
-    const blacklist = ["schnozebot", "fossabot", "biroman", "xqc", "thepositivebot", "darkface____"]; //You might get timed out, for example: darkface____ will time you out because of "racism" thats why I blacklisted his name.
-  
-    chatElements.forEach((chatElement) => {
-      const username = chatElement.textContent.trim();
-  
-      if (username && englishLettersRegex.test(username) && !blacklist.includes(username.toLowerCase())) {
-        uniqueNames.add(username); // Removed "@" here
-      }
-    });
-  
-    const namesLimited = Array.from(uniqueNames).reverse().slice(0, limit);
-    return `${namesLimited.join(" ")} \n\nSOYSCREAM ALERT`;
-  }
 }
-
-new ChatObserver();
+window.addEventListener('load', (event) => {
+  new ChatObserver();
+});
